c0e89dce3dd26d5f7aa75f7cd828795d772507d1
atikahfitriani <atikahfitriani08@gmail.com>
2024-10-07 20:45:10 +0700

    tugas 2 membuat todo list

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:belajar_flutter2/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do App',
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:belajar_flutter2/todo_page.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      if (!mounted) return;
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const TodoPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/splash-animate.json',
+        ),
+      ),
+    );
+  }
+}
